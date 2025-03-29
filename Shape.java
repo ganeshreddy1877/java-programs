241322d65f455da8983a040a3d6bfc9a59ebbe38
ganeshreddy1877 <ganeshreddydharmagari@gmail.com>
2025-03-29 12:15:57 +0530

    Add files via upload

--- Shape.java
+++ Shape.java
@@ -1,39 +1,13 @@
-class Shape {
-    int a, b;
-
-    // Method to calculate area of a square
-    public void calculateArea(int side) {
-        int area = side * side;
-        System.out.println("The area of the square is: " + area);
+public class Shape {
+    public double area(double a){
+        return a*a;
     }
-
-    // Method to calculate area of a rectangle
-    public void calculateArea(int length, int width) {
-        int area = length * width;
-        System.out.println("The area of the rectangle is: " + area);
+    public double area(double a,double b){
+        return a*b;
     }
-}
-
-class Circle extends Shape {
-    int r;
-
-    // Method to calculate area of the circle
-    public void calculateArea() {
-        double area = 3.14 * r * r;
-        System.out.println("The area of the circle is: " + area);
-    }
-}
- class Main {
     public static void main(String[] args) {
-        // Creating objects of each shape and calculating their areas
-        Shape square = new Shape();
-        square.calculateArea(4);  // Example square with side length of 4
-
-        Shape rectangle = new Shape();
-        rectangle.calculateArea(4, 5);  // Example rectangle with length 4 and width 5
-
-        Circle circle = new Circle();
-        circle.r = 3;  // Setting radius of circle to 3
-        circle.calculateArea();  // Calculating area of the circle
+        Shape shape = new Shape();
+        double square=shape.area(a:5);
+        double rectangle=shape.area(a:10,b:20);
     }
 }
